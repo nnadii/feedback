29da1b1b66dd4d7deeff4cbb0fc234fbc9265873
Chimennadi <nnadichime04@gmail.com>
2024-01-13 14:52:47 +0100

    codebase refactorization

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -12,12 +12,8 @@ function FeedbackItem({ item }) {
         <>
             <Card reverse={true}>
                 <div className='num-display'>{item.rating}</div>
-                <button onClick={() => deleteFeedback(item.id)} className='close'>
-                    <FaTimes color='#ff6a95' />
-                </button>
-                <button onClick={() => editFeedback(item)} className='edit'>
-                    <FaEdit color="#ff6a95" />
-                </button>
+                <button onClick={() => deleteFeedback(item.id)} className='close'><FaTimes color='#ff6a95' /></button>
+                <button onClick={() => editFeedback(item)} className='edit'><FaEdit color="#ff6a95" /></button>
                 <div className='text-display'>{item.text}</div>
             </Card>
         </>
